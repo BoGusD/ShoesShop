a8996ad859e4a84031226dcb5b6b069e699de92d
bogus <qazxwd44@hanmail.net>
2023-05-25 17:13:51 +0900

    추가 수정 리펙토링

--- back/routes/shoes.ts
+++ back/routes/shoes.ts
@@ -8,7 +8,7 @@ const handleRequest = async (
   callback: Function
 ) => {
   try {
-    const result = await callback(req.query.gender);
+    const result = await callback();
     res.send(JSON.stringify(result));
   } catch (error) {
     console.error(error);
@@ -17,16 +17,17 @@ const handleRequest = async (
 };
 
 router.get("/", async (req: Request, res: Response) => {
+  console.log("req.query", req.query);
   handleRequest(req, res, mysql.getAllItems);
 });
 
 router.get("/ascPrice", async (req: Request, res: Response) => {
-  const gender = req.query.gender || "";
+  const gender = req.query.gender;
   handleRequest(req, res, mysql.getAscItems.bind(null, gender));
 });
 
 router.get("/descPrice", async (req: Request, res: Response) => {
-  const gender = req.query.gender || "";
+  const gender = req.query.gender;
   handleRequest(req, res, mysql.getDescItems.bind(null, gender));
 });
 
