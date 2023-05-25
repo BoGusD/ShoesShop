0fe19a1b1aeaa81609c7188e6a5879d59b209e89
bogus <qazxwd44@hanmail.net>
2023-05-25 16:53:26 +0900

    코드 리펙토링

--- back/controller/shoesGetController.ts
+++ back/controller/shoesGetController.ts
@@ -4,122 +4,65 @@ const dbClient = require("../service/mysqlConnect");
 const controller = {
   getAllItems: async (): Promise<shoesDataType[]> => {
     try {
-      return new Promise<shoesDataType[]>((resolve, reject) => {
-        dbClient.query(
-          `SELECT * FROM shopping.item;`,
-          (error: Error, rows: shoesDataType[]) => {
-            if (error) {
-              reject(error);
-              return;
-            }
-            resolve(rows);
-          }
-        );
-      });
+      const query = `SELECT * FROM shopping.item;`;
+      const rows = await executeQuery(query);
+      return rows;
     } catch (error) {
       console.error(error);
       throw error;
     }
   },
+
   getAscItems: async (gender: string): Promise<shoesDataType[]> => {
-    console.log("gender", gender);
     try {
-      return new Promise<shoesDataType[]>((resolve, reject) => {
-        if (gender === "male") {
-          dbClient.query(
-            `SELECT * FROM item
-            WHERE gender = 'male'
-            ORDER BY price ASC;
-            `,
-            (error: Error, rows: shoesDataType[]) => {
-              if (error) {
-                reject(error);
-                return;
-              }
-              resolve(rows);
-            }
-          );
-        } else if (gender === "female") {
-          dbClient.query(
-            `SELECT * FROM item
-            WHERE gender = 'female'
-            ORDER BY price ASC;
-            `,
-            (error: Error, rows: shoesDataType[]) => {
-              if (error) {
-                reject(error);
-                return;
-              }
-              resolve(rows);
-            }
-          );
-        } else {
-          dbClient.query(
-            `SELECT * FROM item ORDER BY price ASC`,
-            (error: Error, rows: shoesDataType[]) => {
-              if (error) {
-                reject(error);
-                return;
-              }
-              resolve(rows);
-            }
-          );
-        }
-      });
+      let query = `SELECT * FROM item`;
+      let queryParams: string[] = [];
+
+      if (gender === "male" || gender === "female") {
+        query += ` WHERE gender = ?`;
+        queryParams.push(gender);
+      }
+
+      query += ` ORDER BY price ASC`;
+
+      const rows = await executeQuery(query, queryParams);
+      return rows;
     } catch (error) {
       console.error(error);
       throw error;
     }
   },
+
   getDescItems: async (gender: string): Promise<shoesDataType[]> => {
     try {
-      return new Promise<shoesDataType[]>((resolve, reject) => {
-        if (gender === "male") {
-          dbClient.query(
-            `SELECT * FROM item
-            WHERE gender = 'male'
-            ORDER BY price DESC;
-            `,
-            (error: Error, rows: shoesDataType[]) => {
-              if (error) {
-                reject(error);
-                return;
-              }
-              resolve(rows);
-            }
-          );
-        } else if (gender === "female") {
-          dbClient.query(
-            `SELECT * FROM item
-            WHERE gender = 'female'
-            ORDER BY price DESC;
-            `,
-            (error: Error, rows: shoesDataType[]) => {
-              if (error) {
-                reject(error);
-                return;
-              }
-              resolve(rows);
-            }
-          );
-        } else {
-          dbClient.query(
-            `SELECT * FROM item ORDER BY price DESC`,
-            (error: Error, rows: shoesDataType[]) => {
-              if (error) {
-                reject(error);
-                return;
-              }
-              resolve(rows);
-            }
-          );
-        }
-      });
+      let query = `SELECT * FROM item`;
+      let queryParams: string[] = [];
+
+      if (gender === "male" || gender === "female") {
+        query += ` WHERE gender = ?`;
+        queryParams.push(gender);
+      }
+
+      query += ` ORDER BY price DESC`;
+
+      const rows = await executeQuery(query, queryParams);
+      return rows;
     } catch (error) {
       console.error(error);
       throw error;
     }
   },
 };
+
+const executeQuery = (query: string, params: string[] = []): Promise<any> =>
+  new Promise<any>((resolve, reject) => {
+    dbClient.query(query, params, (error: Error, rows: shoesDataType[]) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(rows);
+    });
+  });
 
 module.exports = { controller };
--- back/routes/shoes.ts
+++ back/routes/shoes.ts
@@ -2,44 +2,37 @@ import express, { Request, Response } from "express";
 const mysql = require("../controller/shoesGetController").controller;
 const router = express.Router();
 
+const handleRequest = async (
+  req: Request,
+  res: Response,
+  callback: Function
+) => {
+  try {
+    const result = await callback(req.query.gender);
+    res.send(JSON.stringify(result));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving items");
+  }
+};
+
 router.get("/", async (req: Request, res: Response) => {
-  try {
-    const result = await mysql.getAllItems();
-    res.send(JSON.stringify(result));
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error retrieving items");
-  }
+  console.log("query", req.query);
+  handleRequest(req, res, mysql.getAllItems);
 });
-router.get("/gender", async (req: Request, res: Response) => {
-  try {
-    const result = await mysql.getAllItems();
-    res.send(JSON.stringify(result));
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error retrieving items");
-  }
-});
+
 router.get("/ascPrice", async (req: Request, res: Response) => {
-  try {
-    const result = await mysql.getAscItems(req.query.gender);
-    res.send(JSON.stringify(result));
-    console.log("result", result);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error retrieving items");
-  }
+  const gender = req.query.gender || "";
+  handleRequest(req, res, mysql.getAscItems.bind(null, gender));
 });
+
 router.get("/descPrice", async (req: Request, res: Response) => {
-  try {
-    const result = await mysql.getDescItems(req.query.gender);
-    res.send(JSON.stringify(result));
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error retrieving items");
-  }
+  const gender = req.query.gender || "";
+  handleRequest(req, res, mysql.getDescItems.bind(null, gender));
 });
+
 router.put("/", (req: Request, res: Response) => {});
+
 router.delete("/", (req: Request, res: Response) => {});
 
 module.exports = router;
