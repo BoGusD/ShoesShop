49a0b0a990ffec29950a7dacd17ea2adcaf334a1
bogus <qazxwd44@hanmail.net>
2023-05-30 18:10:09 +0900

    multer 적용

--- back/routes/shoes.ts
+++ back/routes/shoes.ts
@@ -1,6 +1,27 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, Router } from "express";
+import multer, { StorageEngine } from "multer";
+import fs from "fs";
+
 const mysql = require("../controller/shoesGetController").controller;
 const router = express.Router();
+
+const dir: string = "./shoeImgs";
+const storage: StorageEngine = multer.diskStorage({
+  destination: (req: Request, file: Express.Multer.File, cb) => {
+    cb(null, dir);
+  },
+  filename: (req: Request, file: Express.Multer.File, cb) => {
+    const uniqueSuffix: string =
+      Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const filename: string = file.originalname.replace(/\s/g, "-");
+    cb(null, filename + "-" + uniqueSuffix);
+  },
+});
+const limits: { fileSize: number } = {
+  fileSize: 2048 * 2048 * 2,
+};
+
+const upload = multer({ storage: storage, limits: limits });
 
 const handleRequest = async (
   req: Request,
@@ -24,6 +45,7 @@ router.get("/", async (req: Request, res: Response) => {
     handleRequest(req, res, mysql.getAllItems);
   }
 });
+
 router.get("/ascPrice", async (req: Request, res: Response) => {
   const gender = req.query.gender;
   handleRequest(req, res, mysql.getAscItems.bind(null, gender));
@@ -33,6 +55,38 @@ router.get("/descPrice", async (req: Request, res: Response) => {
   const gender = req.query.gender;
   handleRequest(req, res, mysql.getDescItems.bind(null, gender));
 });
+
+router.post(
+  "/addItem",
+  upload.array("itemImg", 5),
+  (req: Request, res: Response) => {
+    console.log(req.files);
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+    const {
+      id,
+      itemTitle,
+      price,
+      gender,
+    }: { id: string; itemTitle: string; price: string; gender: string } =
+      req.body;
+    const imgSrc: string = req.files
+      ? `${process.env.SERVER_URL}/shoeImgs/${
+          (req.files as Express.Multer.File[])[0]?.filename
+        }`
+      : "";
+
+    const data = {
+      id,
+      itemTitle,
+      price,
+      gender,
+      itemImg: imgSrc,
+    };
+    console.log("data", data);
+
+    handleRequest(req, res, mysql.getDescItems.bind(null, data));
+  }
+);
 
 router.put("/", (req: Request, res: Response) => {});
 
